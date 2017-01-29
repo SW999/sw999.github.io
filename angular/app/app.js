ecd870858c5d15478134667c4d9a1942cbadb1f7
Siarhei Vaitehovich <swarog2002@mail.ru>
2017-01-30 00:02:06 +0300

    Task 8. Add router

--- angular/app/app.js
+++ angular/app/app.js
@@ -1,26 +1,58 @@
 import '../style/main.scss';
 
-let app = () => {
-    return {
-        template: require('./app.html'),
-        controller: 'AppCtrl',
-        controllerAs: 'app'
-    }
-};
-
-class AppCtrl {
-    constructor($http) {
-        $http.get('http://localhost:2992/api/')
-            .then((res) => {
-                this.articles = JSON.parse(res.data.data);
-            });
-    }
-}
-
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
-    .directive('app', app)
-    .controller('AppCtrl', AppCtrl);
+angular.module(MODULE_NAME, [require('angular-route')])
+    .controller('MainCtrl', function ($scope, $route, $routeParams, $location) {
+        $scope.$route = $route;
+        $scope.$location = $location;
+        $scope.$routeParams = $routeParams;
+    })
+    .controller('AppCtrl', function ($scope, $routeParams, articles) {
+        $scope.name = 'ArticleController';
+        $scope.params = $routeParams;
+        $scope.articles = JSON.parse(articles.data.data);
+    })
+
+    .controller('ArticleController', function ($scope, $routeParams, article) {
+        $scope.name = 'ArticleController';
+        $scope.params = $routeParams;
+        $scope.article = JSON.parse(article.data.data);
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                template: require('./articles.html'),
+                controller: 'AppCtrl',
+                resolve: {
+                    articles: function ($q, $http) {
+                        let deferred = $q.defer();
+
+                        $http.get('http://localhost:2992/api/')
+                            .then(function(data) {
+                                deferred.resolve(data);
+                            });
+
+                        return deferred.promise;
+                    }
+                }
+            })
+            .when('/article/:alias', {
+                template: require('./article.html'),
+                controller: 'ArticleController',
+                resolve: {
+                    article: function ($q, $http, $location) {
+                        let deferred = $q.defer();
+
+                        $http.get('http://localhost:2992/api' + $location.path().replace(/\/article/gi, ''))
+                            .then(function(data) {
+                                deferred.resolve(data);
+                            });
+
+                        return deferred.promise;
+                    }
+                }
+            });
+    });
 
 export default MODULE_NAME;
