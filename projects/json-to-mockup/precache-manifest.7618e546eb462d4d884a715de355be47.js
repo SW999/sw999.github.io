274e9268e3d2efc762259628b6303ed9a3c8479f
Siarhei Vaitehovich <swarog2002@mail.ru>
2018-11-30 20:36:22 +0300

    master. Update the JSON to mock-up project

---
+++ projects/json-to-mockup/precache-manifest.7618e546eb462d4d884a715de355be47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66254b3d0bb8f96d9f5e9bf26410b53",
+    "url": "/static/media/broken_heart.b66254b3.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0a11d026f66ef032b02",
+    "url": "/static/js/main.a0a11d02.chunk.js"
+  },
+  {
+    "revision": "e0587f3714af5528d23a",
+    "url": "/static/js/1.e0587f37.chunk.js"
+  },
+  {
+    "revision": "a0a11d026f66ef032b02",
+    "url": "/static/css/main.c73ff668.chunk.css"
+  },
+  {
+    "revision": "80402f310a066ffc4c79a2832936e557",
+    "url": "/index.html"
+  }
+];
