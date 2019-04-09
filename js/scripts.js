1218f506004f0e2251906589e348f840eb92edf6
Siarhei Vaitehovich <swarog2002@mail.ru>
2019-04-09 16:59:01 +0300

    Add title to the Main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 const d = document;
 const $toggleMenu = d.querySelector('.menu-toggle');
+const $title = d.querySelector('.title-section');
 
 $toggleMenu.addEventListener('click', () => {
   $toggleMenu.classList.toggle('on');
+  $title.classList.toggle('hide');
   d.querySelector('.menu-section').classList.toggle('on');
   d.querySelector('nav ul').classList.toggle('hidden');
 });
