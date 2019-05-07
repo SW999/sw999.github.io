61241bc4c43bf392f10c53f9df95e2d970b9bff7
Siarhei Vaitehovich <swarog2002@mail.ru>
2019-05-07 22:15:17 +0300

    Add removeAddressBar function for mobile devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 const d = document;
 const $toggleMenu = d.querySelector('.menu-toggle');
 const $title = d.querySelector('.title-section');
+const isMobileDevice = /(iphone|ipod|ipad|android)/gi.test(navigator.userAgent);
+const removeAddressBar = () => setTimeout(window.scrollTo(0, 1), 10);
+
+if(isMobileDevice) {
+  window.addEventListener('load', removeAddressBar, false);
+  window.addEventListener('orientationchange', removeAddressBar, false);
+}
 
 $toggleMenu.addEventListener('click', () => {
   $toggleMenu.classList.toggle('on');
