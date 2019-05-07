8b76e1ebc2bea6e56edb86573ee66880a913f5cd
Siarhei Vaitehovich <swarog2002@mail.ru>
2019-05-07 23:46:43 +0300

    Add bullshit thanks to mobile Google Chrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 const d = document;
 const $toggleMenu = d.querySelector('.menu-toggle');
 const $title = d.querySelector('.title-section');
+const isAndroid = /(android)/gi.test(navigator.userAgent);
+
+if (isAndroid) {
+  d.body.classList.add('os-android');
+}
 
 $toggleMenu.addEventListener('click', () => {
   $toggleMenu.classList.toggle('on');
