d5bfc1999b7f446b6608d34cf416228e9b2fa5bd
MINSK\Siarhei_Vaitehovich <Siarhei_Vaitehovich@epam.com>
2022-01-15 18:07:54 +0200

    Simplify styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,4 @@
-const d = document;
-const $toggleMenu = d.querySelector('.menu-toggle');
-const $title = d.querySelector('.title-section');
-const userAgent = navigator.userAgent;
+const menu = document.querySelector('.menu');
+const toggleMenu = document.querySelector('.menu-toggle');
 
-if (/(android|iphone)/gi.test(userAgent) && /(chrome|safari)/gi.test(userAgent)) {
-  d.body.classList.add('mobile-slug');
-}
-
-$toggleMenu.addEventListener('click', () => {
-  $toggleMenu.classList.toggle('on');
-  $title.classList.toggle('hide');
-  d.querySelector('.menu-section').classList.toggle('on');
-  d.querySelector('nav ul').classList.toggle('hidden');
-});
+toggleMenu.addEventListener('click', () => menu.classList.toggle('on'));
